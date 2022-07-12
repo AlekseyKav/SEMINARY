7052a8e581c9c828eb00e1309ad1fb1ce008ea06
ALeksey <nor-man@yandex.ru>
2022-07-12 21:53:50 +0300

    add ZAD_3 & 4

---
+++ ZADACHA_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+Console.WriteLine("Введите число 1 ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2 ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1%num2 == 0)
+{
+    Console.WriteLine("кратно")
+    }
+    else{
+        Console.WriteLine("Остаток: " + num1%num2)
+    }
---
+++ ZADACHA_4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+
+if ((num%7 == 0)&&(num%23 == 0))
+{
+    Console.WriteLine("кратно");
+    }
+   Console.WriteLine("Не кратно");
+
+   /*
+   // Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+if ((num%7 == 0)&&(num%23 == 0))
+{
+    Console.WriteLine("Кратно");
+}
+Console.WriteLine("Не кратно");
+*/
